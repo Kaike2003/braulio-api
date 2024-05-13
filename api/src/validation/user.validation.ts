cdb36842c1d2edbe597c8529766afb560294ac09
Kaike Bartolomeu <102388705+Kaike2003@users.noreply.github.com>
2024-05-13 11:24:56 +0100

    base de dados terminada

--- api/src/validation/user.validation.ts
+++ api/src/validation/user.validation.ts
@@ -1,4 +1,13 @@
 import { z } from "zod"
 
 
-const Schema =
+const SchemaUser = z.object({
+    id: z.string()
+        .min(30)
+        .max(35),
+    username: z.string()
+        .min(3)
+        .max(45),
+    email: z.string()
+        .email()
+})
